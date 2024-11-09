38828d3406b7c813e7610ffbc47c9afa2dc52183
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-11-09 18:56:27 +0530

    Port Assignment

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
 const port  = process.env.PORT || 3000; //YE JO PROCESS WALI LINE HAI PORT KOI BHI ACCEPT KAR LEGI IF IT RUN ON ANOTHER MACHINE
+
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
+})
