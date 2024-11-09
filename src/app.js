c9184bcc3375de0913a13fd5aeb7c8fac4d4dae8
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-11-10 01:30:29 +0530

    Nodemon

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port  = process.env.PORT || 3000; //YE JO PROCESS WALI LINE HAI PORT KOI BHI ACCEPT KAR LEGI IF IT RUN ON ANOTHER MACHINE
 
+app.get("/",(req,res) =>{
+    res.send("Oye! Punjabi aa gaye oye");
+})
+
 app.listen(port, () => {
     console.log(`server is running at port no ${port}`);
 })
