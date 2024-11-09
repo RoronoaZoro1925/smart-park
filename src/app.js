2c7cc461c02f6cd018c50166183d060b19cd8552
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-11-09 16:29:19 +0530

    App.js Creation

---
+++ src/app.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const app = express();
+const port  = process.env.PORT || 3000; //YE JO PROCESS WALI LINE HAI PORT KOI BHI ACCEPT KAR LEGI IF IT RUN ON ANOTHER MACHINE
