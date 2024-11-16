8e6c1be181228b94ac556d56c0d80d69c0887f66
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-11-16 20:59:24 +0530

    connection of database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port  = process.env.PORT || 3000; //YE JO PROCESS WALI LINE HAI PORT KOI BHI ACCEPT KAR LEGI IF IT RUN ON ANOTHER MACHINE
+require("./db/conn");
+
+console.log(path.join(__dirname, "../public"))
+
+// const static_path = path.join(__dirname, "../public");
+
+// app.use(express.static(static_path))
 
 app.get("/",(req,res) =>{
     res.send("Oye! Punjabi aa gaye oye");
