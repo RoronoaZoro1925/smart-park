1bddef8717383dcb20be2e786723eb0f40c1bee0
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-11-17 15:25:28 +0530

    Index.html

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const app = express();
 const port  = process.env.PORT || 3000; //YE JO PROCESS WALI LINE HAI PORT KOI BHI ACCEPT KAR LEGI IF IT RUN ON ANOTHER MACHINE
 require("./db/conn");
 
-console.log(path.join(__dirname, "../public"))
+const static_path = path.join(__dirname, "../public");
 
-// const static_path = path.join(__dirname, "../public");
+// console.log(path.join(__dirname, "../public"));
 
-// app.use(express.static(static_path))
+app.use(express.static(static_path));
 
-app.get("/",(req,res) =>{
-    res.send("Oye! Punjabi aa gaye oye");
-})
+// app.get("/",(req,res) =>{
+//     res.send("Oye! Punjabi aa gaye oye");
+// })
 
 app.listen(port, () => {
     console.log(`server is running at port no ${port}`);
